5e7984a67987f20afc5089b55807609a954a1997
Ítalo Moura <italo.moura1201@gmail.com>
2019-02-28 14:20:56 -0300

    Smart SM

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,20 @@
 module.exports = function(app) {
  	var IndexController = {
 		index: function(req,res) {
-			res.render('index', { title: 'Super IT' });
+			res.render('index', { title: 'Smart SM' });
 		},
 		compra: function(req,res) {
-			res.render('compra', { title: 'Super IT' });
+			res.render('compra', { title: 'Smart SM' });
 		},
 		lista: function(req,res) {
-			res.render('lista', { title: 'Super IT' });
+			res.render('lista', { title: 'Smart SM' });
 		},
 		parceiros: function(req,res) {
-			res.render('parceiros', { title: 'Super IT' });
+			res.render('parceiros', { title: 'Smart SM' });
 		},
 		sobre: function(req,res) {
-			res.render('sobre', { title: 'Super IT' });
+			res.render('sobre', { title: 'Smart SM' });
 		}
-	}	
+	}
 	return IndexController;
 }
